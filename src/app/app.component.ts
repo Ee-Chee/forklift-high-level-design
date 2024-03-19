4fe09f1822c9f0d5eba195abfc274dd40697afad
Leng <eechee.leng@gmail.com>
2024-03-20 00:13:47 +0100

    add best practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet,],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
